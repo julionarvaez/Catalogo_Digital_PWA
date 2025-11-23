5145fc9fedf306f34b76f8b39ddc6f1facbf2b85
julionarvaez <juliocesarnarvaez30@gmail.com>
2025-11-23 15:13:14 -0500

    no message

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // === SERVICE WORKER PARA ALIMENTO DEL CIELO PWA ===
 // IMPORTANTE: Incrementar esta versión cuando haya cambios importantes
-const CACHE_VERSION = '1.0.12';
+const CACHE_VERSION = '1.0.16';
 const CACHE_NAME = `alimento-del-cielo-v${CACHE_VERSION}`;
 const APP_VERSION = CACHE_VERSION;
 
